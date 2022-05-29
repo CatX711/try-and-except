2226c10abc287fd395259b649b410e1b7efbdb47
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-05-29 21:20:02 +0100

    inital commit

---
+++ try and except.py
@@ -0,0 +1,23 @@
+
+
+try:
+    def intEasylen(number):
+        int((number))
+        print(len(number))
+        return
+    number = (input("Type a number. "))
+    intEasylen(number)
+except:
+    print("Invalid option.")
+
+
+
+
+
+
+
+
+
+
+
+
